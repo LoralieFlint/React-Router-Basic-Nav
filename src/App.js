f114d56d44de1c09adccdcd8b7381941fdfe8c39
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-09-03 21:43:22 -0600

    project complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Home, About, Contact, Navigation } from "./components";
+import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
+    <Route exact path="/" />
+    <Route path="/about" />
+    <Route path="/contact" />
     <Navigation />
   </div>
 );
